018c8dc92361750f979e515df04fae4c2d75f5bc
Anthony Cox <motorhead1991@hotmail.com>
2015-05-09 00:00:55 -0600

    Initial commit: 64bit kernel toolchain based on pure GCC 4.9.2

---
+++ lib/gcc/aarch64-linux/4.9.2/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-4.9.2/configure --prefix=/home/motorhead/toolchain/arm64-4.9.2 --target=aarch64-linux --enable-languages=c,c++ --disable-multilib --disable-werror";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
